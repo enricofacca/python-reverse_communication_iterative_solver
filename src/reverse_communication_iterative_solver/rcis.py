fad6ae26bf751ac3a37c260e843e5d61aee806ca
enricofacca <enrico.facca@sns.it>
2021-12-31 17:44:14 +0100

    working example

---
+++ src/reverse_communication_iterative_solver/rcis.py
@@ -0,0 +1,388 @@
+"""
+Python module defining a template for iterative algorithm,
+which means algorithm that incrementaly approach the 
+solution of a problem. In this project this
+increment is called "update".
+
+We decompose this kind of algorithms into 4 main steps
+where the user has a specific task:
+1 - set the solver controls ( and eventuality problem inputs)
+    before the next update. 
+2 - reset the solver controls (and eventuality reset problem inputs)
+    after a solver failure 
+3 - study any quantity defined by the problem, unknows, and solver
+4 - set when the solver has finished its job.
+
+This decomposition is done via a Reverse Communication (RC) 
+approach, an old style of programming (used for 
+example in the ARPACK library). While Python is so flexible 
+that other approaches could have been used, we adopt it
+because it let the user work "manually" on the structures
+describing its problem instead of having to define in advance 
+the procedure to handle the steps 1,2,3,4.
+
+This is particularly convient in early step of
+the algorithm development. A nicer and clenear 
+function hiding the RC cycle to an user who 
+wants a block-box solver can be easily build.
+
+Algorithm and problem descricing are separate 
+with Object Orientied programming style 
+introducing 3 (+1) classes:
+
+Problem: A class that should describe the problem we want to solve
+Unknows: A class describing the solution of the problem
+
+These two class are left empty, see simple_example.py to have a
+concrete example.
+
+Solver: An abstract class containg the abstact procedure "update"
+acticing on unknows with the class problem
+
+ConstrainedSolver: An abstract class extending the Solver class (so
+also the procudure "update" is included) with a procudere "syncronize"
+to transform the unknows so that some problem constraints are
+satissfied.
+
+
+"""
+from abc import ABC, abstractmethod
+import time as cputiming
+
+
+class Solver(ABC): 
+    """
+    Abstract procedure describing the update action of the 
+    solver on the unknows.
+
+    Args:
+    problem : class describing problem setup
+    unknows : class describing problem solution
+
+    Return:
+    unknows : update solution after one update
+    ierr    : integer ==0 if no error occurred.
+              Values different from 0 are used to
+              identifity the error occured
+    self    : solver class. We return the solver
+              to read statistic of the last update.
+    """
+    @abstractmethod
+    def update(self, problem, unknows):
+        return unknows,ierr,self;
+
+
+class ConstrainedSolver(ABC): 
+    """
+    Abstract procedure describing the solver syncronizing i.e.,
+    action of the solver on the problem unknows so they fit 
+    into potential problem constraints. 
+    
+    Args:
+    problem : class describing problem setup
+    unknows : class describing problem solution
+
+    Return:
+    unknows : update solution after one update
+    ierr    : integer ==0 if no error occurred.
+              Values different from 0 are used to
+              identifity the error occured
+    self    : solver class. We return the solver
+              to read statistic.
+    """
+    @abstractmethod
+    def syncronize(self, problem, unknows):
+        return unknows,ierr,self;
+
+# Two (empty) abstact classes that will contain
+# respectively problem unknows and inputs.
+# They will both contain only pure data (like integer, real,complex).
+# Unknows will store for example the coefficient of the
+# solution of a PDE.
+# Inputs will contain the coefficient of the forcing term in a PDE.
+class Unknows(ABC):
+    NumberOfUnknows=0;
+class Problem(ABC):
+    NumberOfInputs=0;
+    
+    
+class CycleControls():
+    """
+    Reverse communication controls and info.
+    (Here we keep controls and info togheter for having just one class)
+    """
+    def __init__(self,
+                 max_iterations=100,
+                 max_restart_update=10,
+                 verbose=0):
+        # State variable of iteration
+        # Order comunication flag 
+        self.flag=0
+        # State comunication flag
+        self.info=0
+        # Update counter
+        self.iterations=0
+        # Restart counter
+        self.nrestart_update=0
+        
+        
+        # Controls of iterative algorithm
+        # maximum number of updates
+        self.max_iterations=max_iterations
+        # maximum number of restart
+        self.max_restart_update=max_restart_update
+        # verbose
+        self.verbose=verbose
+        
+        # Statistics of iterative algorithm
+        # Cpu conter
+        self.cpu_time=0
+        
+    def reverse_communication_solver(self, solver, problem, unknows):
+        """ 
+        Subroutine to run reverse communition approach 
+        of iterative solver.
+        
+        Args:
+        solver(inout): class(Solver) with method syncronize and update
+        ctrls (in   ): Solver controls
+        dmkin (in   ): class(Problem) containg current realization of problem
+        unknows (inout): Problem unknows
+        """
+    
+        if (self.flag == 0):
+            # study system
+            self.flag=3
+            self.ierr=0                
+            return self, unknows,solver;
+    
+        if (self.flag == 2 ):
+            # user has settled problem inputs and controls
+            # now we update
+            self.ierr=0
+            
+            # Update cycle. 
+            # If it succees goes to the evaluation of
+            # system varaition (flag ==4 ).
+            # In case of failure, reset controls
+            # and ask new problem inputs or ask the user to
+            # reset controls (flag == 2 + ierr=-1 ).
+
+            print(self.verbose)
+            if (  self.nrestart_update == 0):
+                if (self.verbose==1):
+                    print(' ')
+                    print('UPDATE '+str(self.iterations))
+                cpu_update=0.0
+            else:
+                if (self.verbose>=1):
+                    print('UPDATE '+
+                          str(self.time_iterations+1)+
+                          ' | RESTART = '+
+                          str(self.nrestart_update))
+            #
+            # update unknows
+            #
+            start_time = cputiming.time()
+            [unknows,ierr,solver]=solver.update(problem, unknows)
+            cpu_update += cputiming.time()-start_time
+        
+            #
+            # check for successfulls update
+            #
+            if ( ierr == 0):
+                # info for succesfull update
+                self.iterations+=1
+                self.cpu_time+=cputiming.time()-start_time
+                if (self.verbose >=1 ):
+                    if ( self.nrestart_update == 0):
+                        print ('UPDATE SUCCEED CPU =',
+                               '{:.2f}'.format(cpu_update))
+                    else:
+                        print('UPDATE SUCCEED '+
+                              str(self.nrestart_update) +
+                              ' RESTARTS CPU =',
+                              '{:.2f}'.format(cpu_update))
+                        print(' ')
+                #
+                # Ask to the user to evalute if stop cycling 
+                # 
+                # 
+                self.flag       = 4 
+                self.ierr       = 0 
+            else:
+                #
+                # update failed
+                #
+                if (self.verbose>=1):
+                    print('UPDATE FAILURE')
+            
+                # try one restart more
+                self.nrestart_update += 1
+            
+                # stop if number max restart update is passed 
+                if ( self.nrestart_update >= self.max_restart_update):
+                    self.flag       = -1  # breaking cycle
+                    self.ierr       = -1 
+                else:
+                    # ask the user to reset controls and problem inputs
+                    self.flag =  2
+                    self.ierr = -1
+            return self, unknows,solver;
+       
+        if (self.flag ==3):
+            # check if maximum number iterations was achieded 
+            if ( self.iterations >= self.max_iterations):
+                self.flag = -1
+                self.ierr = -1
+                if (self.verbose>=1):
+                    print(' Update Number exceed limits'+
+                          str(self.max_iterations))
+                # break cycle
+                return self, unknows,solver;
+
+            # New update is required thus
+            # ask for new controls
+            unknows.nrestart_update = 0 # we reset the count of restart
+            self.flag = 2
+            self.ierr = 0
+            return self, unknows,solver;
+
+        if (self.flag == 4):
+            # ask user new problem inputs in order to update 
+            self.flag = 3
+            self.ierr = 0
+            return self, unknows,solver;
+
+       
+    def reverse_communication_constrained_solver(self,
+                                                 solver,
+                                                 problem,
+                                                 unknows):
+        """ 
+        Subroutine to run reverse communition approach 
+        of constrained solver.
+        
+        Args:
+        solver(inout): class(Solver) with method syncronize and update
+        ctrls (in   ): Solver controls
+        dmkin (in   ): class(Problem) containg current realization of problem
+        unknows (inout): Problem unknows
+        infos (inout): Solver statistics and info
+        """
+    
+        if (self.flag == 0):
+            # syncronize
+            start_time = cputiming.time()            
+            [unknows,ierr,solver]=solver.syncronize(problem, unknows)
+            syncr_time=cputiming.time()-start_time
+            self.cpu_time+=syncr_time
+
+            if (self.verbose>=1):
+                print('SYNCRONIZE AT TIME: CPU', '{:.2f}'.format(syncr_time))
+            if ( ierr == 0):
+                # study system
+                self.flag=3
+                self.ierr=0
+            else:
+                self.flag=-2
+                self.ierr=0
+                
+            return self, unknows,solver;
+    
+        if (self.flag == 2 ):
+            # user has settled problem inputs and controls
+            # now we update
+            self.ierr=0
+            
+            # Update cycle. 
+            # If it succees goes to the evaluation of
+            # system varaition (flag ==4 ).
+            # In case of failure, reset controls
+            # and ask new problem inputs or ask the user to
+            # reset controls (flag == 2 + ierr=-1 ).
+        
+            if (  self.nrestart_update == 0):
+                if (self.verbose>=1):
+                    print(' ')
+                    print('UPDATE '+str(self.iterations))
+                cpu_update=0.0
+            else:
+                print('UPDATE '+
+                      str(self.time_iterations+1)+
+                      ' | RESTART = '+
+                      str(self.nrestart_update))
+            #
+            # update unknows
+            #
+            start_time = cputiming.time()
+            [unknows,ierr,solver]=solver.update(problem, unknows)
+            cpu_update += cputiming.time()-start_time
+        
+            #
+            # check for successfulls update
+            #
+            if ( ierr == 0):
+                # info for succesfull update
+                self.iterations+=1
+                self.cpu_time+=cputiming.time()-start_time
+                if (self.verbose>=1):                    
+                    if ( self.nrestart_update == 0):
+                        print ('UPDATE SUCCEED CPU =',
+                               '{:.2f}'.format(cpu_update))
+                    else:
+                        print('UPDATE SUCCEED '+
+                              str(self.nrestart_update) +
+                              ' RESTARTS CPU =','{:.2f}'.format(cpu_update))
+                        print(' ')
+                #
+                # Ask to the user to evalute if stop cycling 
+                # 
+                # 
+                self.flag       = 4 
+                self.ierr       = 0 
+            else:
+                #
+                # update failed
+                #
+                print('UPDATE FAILURE')
+            
+                # try one restart more
+                self.nrestart_update += 1
+            
+                # stop if number max restart update is passed 
+                if ( self.nrestart_update >= self.max_restart_update):
+                    self.flag       = -1  # breaking cycle
+                    self.ierr       = -1 
+                else:
+                    # ask the user to reset controls and problem inputs
+                    self.flag =  2
+                    self.ierr = -1
+            return self, unknows, solver;
+       
+        if (self.flag ==3):
+            # check if maximum number iterations was achieded 
+            if ( self.iterations >= self.max_iterations):
+                self.flag = -1
+                self.ierr = -1
+                if (self.verbose>=1):
+                    print(' Update Number exceed limits'+
+                          str(self.max_iterations))
+                # break cycle
+                return self, unknows,solver;
+
+            # New update is required thus
+            # ask for new controls
+            unknows.nrestart_update = 0 # we reset the count of restart
+            self.flag = 2
+            self.ierr = 0
+            return self, unknows,solver;
+
+        if (self.flag == 4):
+            # ask user new problem inputs in order to update 
+            self.flag = 3
+            self.ierr = 0
+            return self, unknows, solver;
+
+        
+        
---
+++ tests/my_test.py
@@ -0,0 +1,194 @@
+import sys
+import os
+from copy import deepcopy as cp
+
+
+sys.path.append(os.path.abspath(
+    '../src/reverse_communication_iterative_solver/'))
+
+#from rcis import *
+from reverse_communication_iterative_solver.rcis import *
+
+class Real(Unknows):
+    """
+    We extend the Unknows class.
+    We add a method for saving the data to file. 
+    """
+    def __init__(self,x0):
+        """
+        Constructor setting initial solution
+        """
+        self.NumberOfUnknows=1
+        self.x=x0
+        self.time=0.0
+        self.iteration=0
+    def Save(self,file):
+        """
+        Procedure for saving to file
+        """        
+        f.open(file,'w')
+        f.write(self.x)
+        f.close()
+        return info;
+
+class Parabola(Problem):
+    """
+    Problem inputs with the coefficient of the parabola
+    y=a*x^2/2
+    """
+    def __init__(self,a,lb=-1e30,up=1e30):
+        self.a=a
+        self.lower_bound=lb
+        self.upper_bound=up
+
+
+class GradientDescentControls:
+    """
+    Class with gradient decent controls. 
+    
+    """
+    def __init__(self,
+                 step0=0.1,
+                 verbose=0):
+        """
+        Step Length
+        """
+        # step length
+        self.step=step0
+
+        # info solver application
+        self.verbose=0
+
+class InfoDescent:
+    def __init__(self):
+        self.cpu_gradient=0.0
+
+class ParabolaDescent(ConstrainedSolver): 
+    """
+    We extend the class "Solver"
+    ovverriding the "syncronize" and the "update"
+    procedure. 
+    """
+    def __init__(self, ctrl=None):
+        """
+        Initialize solver with passed controls (or default)
+        and initialize structure to store info on solver application
+        """
+        # init controls
+        if ( ctrl==None):
+            self.ctrl=GradientDescentControls(0.1)
+        else:
+            self.ctrl=cp(ctrl)
+        # init infos
+        self.infos=InfoDescent()
+            
+    def syncronize(self, problem, unknows):
+        """
+        Since the there are no constrain
+        the first procedure does nothing.
+        """
+
+        # example of how the setup of our problem
+        # can influence solver execution
+        if ( unknows.x>=problem.lower_bound and unknows.x<=problem.upper_bound) :
+            ierr=0
+        else:
+            ierr=-1
+            
+        return unknows,ierr,self;
+
+    def update(self, problem, unknows):
+        """
+        The update is one step of gradient descent.
+        Currently with explicit euler.
+        """
+        start_time = cputiming.time() 
+        gradient_direction=-problem.a*unknows.x
+        self.infos.cpu_gradient=cputiming.time()-start_time+unknows.iteration
+        
+        unknows.x=unknows.x+self.ctrl.step*gradient_direction
+        unknows.time+=self.ctrl.step
+        
+        # example of how the setup of our problem
+        # can influence solver execution
+        if ( unknows.x>=problem.lower_bound and unknows.x<=problem.upper_bound) :
+            ierr=0
+        else:
+            ierr=-1
+
+        self.test=unknows.x
+        
+        return unknows,ierr,self;
+
+def test_main():
+    # init solution container and copy it
+    sol=Real(1)
+
+
+    # init inputs data
+    data=Parabola(0.5)
+
+
+    # init solver
+    ctrl=GradientDescentControls(0.5)
+    gradient_descent=ParabolaDescent(ctrl)
+
+    # init update cycle controls
+    flags=CycleControls(100,verbose=0)
+
+    # list to store 
+    sol_old=cp(sol)
+    hystory=[]
+    while(flags.flag >=0):
+        ###################################################
+        # Call reverse communication.
+        # Then select action according to flag.flag and flag.info
+        ######################################################
+        flags, sol, gradient_descent=flags.reverse_communication_constrained_solver(
+            gradient_descent,
+            data,
+            sol)
+    
+        # set problem inputs and controls before update 
+        if (flags.flag == 2) and (flags.info==0) :
+            #print('time to fix inputs and controls '+
+            #      'for next update')
+            
+            # copy data before update
+            sol_old.x=cp(sol.x)
+            
+        ###################################################
+        # set inputs and reset controls due to failure
+        ###################################################
+        if (flags.flag == 2) and not (flags.info==0) :
+            #########################################################
+            # Do what ever you want of with the data
+            # without modyfyng them
+            ########################################################
+            pass
+        if ( flags.flag == 3):
+            #print('time study the system')
+            #print('sol=', sol.x,'energy=',data.a*sol.x**2/2.0)
+            #print('iter=',flags.iterations)
+            sol.iteration=cp(flags.iterations)
+            hystory.append([sol.time,cp(gradient_descent.infos)])
+        
+        
+        #######################################################
+        # Define you stop criteria and set flags.flag=-1
+        # to exit from the cycle. Set flags.info=0
+        # if you reached your scope. 
+        ########################################################
+        if ( flags.flag == 4):
+            # evalute system variation
+            var=abs(sol.x-sol_old.x)/gradient_descent.ctrl.step
+            #print( 'var=',var,'old=',sol_old.x,'new',sol.x)
+            # break cycle if convergence is achieved 
+            if ( var < 1e-4):
+                flags.flag=-1
+                flags.info=0
+        
+    assert (abs(sol.x)<1e-3) 
+
+if __name__ == "__main__":
+    sys.exit(test_main())
