4635fe704a84dec2bac092df74d3c94c5abcf9c1
enricofacca <enrico.facca@sns.it>
2022-01-01 14:15:16 +0100

    testing docs in __init__.py

--- src/reverse_communication_iterative_solver/__init__.py
+++ src/reverse_communication_iterative_solver/__init__.py
@@ -1,3 +1,21 @@
 __version__ = '0.1.0'
 
 from .rcis import *
+
+
+def empty_function():
+    """
+    This function is useless
+    """
+    return 0
+
+class empy_funciton:
+    """
+    This class is useless
+    """
+    def __init__(i):
+        """
+        This init is useless
+        """
+        self.i = i
+        
